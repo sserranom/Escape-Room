980f9c0712c1add1dcdd9d3a94125b0d3ba76b99
Eduardo Zavarce <eduzavarce@gmail.com>
2025-05-08 12:58:37 +0200

    Connection to MySQL Service

---
+++ src/main/java/cat/itacademy/escaperoom/Main.java
@@ -0,0 +1,46 @@
+package cat.itacademy.escaperoom;
+
+
+import cat.itacademy.escaperoom.database.MySqlConnection;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Statement stmt = null;
+        ResultSet rs = null;
+
+
+        Connection conn = MySqlConnection.getInstance();
+
+        try {
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM patata");
+
+            // or alternatively, if you don't know ahead of time that
+            // the query will be a SELECT...
+
+            if (stmt.execute("SELECT * FROM patata")) {
+                rs = stmt.getResultSet();
+                System.out.println("ResultSet");
+                System.out.println(rs.next());
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
---
+++ src/main/java/cat/itacademy/escaperoom/database/MySqlConnection.java
@@ -0,0 +1,36 @@
+package cat.itacademy.escaperoom.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySqlConnection {
+    private static final String CONNECTION_STRING = MySqlConfig.getString();
+    private static volatile Connection instance;
+
+
+    private MySqlConnection() {
+    }
+
+    private static Connection createConnection() throws SQLException {
+        return DriverManager.getConnection(MySqlConnection.CONNECTION_STRING);
+    }
+
+    public static Connection getInstance() {
+        Connection result = instance;
+        if (result != null) {
+            return result;
+        }
+        synchronized (MySqlConnection.class) {
+            if (instance == null) {
+                try {
+                    instance = createConnection();
+                } catch (SQLException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+
+            return instance;
+        }
+    }
+}
