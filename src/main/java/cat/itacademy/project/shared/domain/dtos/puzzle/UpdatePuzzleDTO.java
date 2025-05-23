b20c59800664d3a897fa866410bbfc61e0c31f9c
Eduardo Zavarce <eduzavarce@gmail.com>
2025-05-23 18:37:07 +0200

    price dtos

--- src/main/java/cat/itacademy/project/shared/domain/dtos/puzzle/UpdatePuzzleDTO.java
+++ src/main/java/cat/itacademy/project/shared/domain/dtos/puzzle/UpdatePuzzleDTO.java
@@ -1,6 +1,7 @@
 package cat.itacademy.project.shared.domain.dtos.puzzle;
 
 import cat.itacademy.project.shared.domain.exceptions.EmptyFieldException;
+import cat.itacademy.project.shared.domain.exceptions.InvalidPriceException;
 
 public record UpdatePuzzleDTO (String nameToUpdate, String name, String difficulty, int roomId, String answer, String story, int themeId, double price)  {
     public UpdatePuzzleDTO {
@@ -11,6 +12,12 @@ public record UpdatePuzzleDTO (String nameToUpdate, String name, String difficul
 
         if((name == null || name.isBlank())){
             throw new EmptyFieldException("The name of the puzzle must be provided.");
+        }
+        if (price <= 0) {
+            throw new InvalidPriceException("Price must be greater than 0");
+        }
+        if (price > 99999999.99){
+            throw new InvalidPriceException("Price must be less than 99999999.99");
         }
 
 
